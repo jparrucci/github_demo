00d3adf80784c372d962f44d8cd889f55d37d4ad
jparrucci <38542296+jparrucci@users.noreply.github.com>
2018-08-25 13:56:27 -0500

    starter file

---
+++ Cereal.py
@@ -0,0 +1,12 @@
+import os
+import csv
+cereal_csv = os.path.join("../Desktop", "cereal.csv")
+
+with open(csv_csv, newline="") as cereal_file:
+    csv_reader = csv.reader(cereal_file, delimiter=",")
+    print(next(csv_reader, None))
+
+    for row in csv_reader
+        if float(row[7] >= 5:
+            print(row)  
+
