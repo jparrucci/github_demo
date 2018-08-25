9add3194fde9f0693ece5b40783c503ccf7d674d
jparrucci <38542296+jparrucci@users.noreply.github.com>
2018-08-25 11:57:27 -0700

    Add files via upload

---
+++ Hobby.py
@@ -0,0 +1,11 @@
+my_dict = {
+    "name": "jessica",f
+    "age": "36",
+    "hobbies": ["football", "running"]
+    "wake up times" : {
+            "monday": "5:30",
+            "tuesday": "5:15"
+    }
+}
+
+print(str(my_dict["""]})
